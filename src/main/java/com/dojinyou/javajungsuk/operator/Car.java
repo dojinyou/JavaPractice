3e91fedd167839b7567061e4990c8ca448c39502
dojinyou <djyou128@gmail.com>
2022-02-03 22:17:48 +0900

    other: configure file auto update

--- src/main/java/com/dojinyou/javajungsuk/operator/Car.java
+++ src/main/java/com/dojinyou/javajungsuk/operator/Car.java
@@ -5,9 +5,11 @@ public class Car{
     String name;
     int number;
 
-    //toString을 오버라이드 해 보세요.
-    //return 형식은 아래줄을 참고하세요:
-    //"name: " + name + ", number: " + number;
+    /*
+    toString을 오버라이드 해보세요
+    return 형식은 아래 줄을 참고하세요.
+    "name: " + name + ", number: " + number;
+    */
 
     @Override
     public String toString() {
