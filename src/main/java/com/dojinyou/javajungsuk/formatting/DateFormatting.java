83a4e2841d420c5bcff7fdc8fd5ae76d98b54edf
dojinyou <djyou128@gmail.com>
2022-02-05 15:33:28 +0900

    Feat: Date formatting Exam
    SimpleDateFormat을 이용하여 간단하게 날짜와 시간을 원하는 포맷으로 표현하는 연습

---
+++ src/main/java/com/dojinyou/javajungsuk/formatting/DateFormatting.java
@@ -0,0 +1,26 @@
+package com.dojinyou.javajungsuk.formatting;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormatting {
+    public static void main(String[] args) {
+        Date today = new Date();
+
+        SimpleDateFormat sdf1,sdf2,sdf3,sdf4;
+
+        sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+        sdf2 = new SimpleDateFormat("yyyy년 MM월 dd일 a hh:mm:ss");
+        sdf3 = new SimpleDateFormat("yyyy년 MM월 dd일 HH:mm:ss");
+        sdf4 = new SimpleDateFormat("yyyy년 MM월 dd일 E요일 hh:mm:ss.SSS");
+
+        System.out.println("sdf1 = " + sdf1.format(today));
+//      sdf1 = 2022-02-05
+        System.out.println("sdf2 = " + sdf2.format(today));
+//      sdf2 = 2022년 02월 05일 오후 03:29:41
+        System.out.println("sdf3 = " + sdf3.format(today));
+//      sdf3 = 2022년 02월 05일 15:29:41
+        System.out.println("sdf4 = " + sdf4.format(today));
+//      sdf4 = 2022년 02월 05일 토요일 03:29:41.239
+    }
+}
