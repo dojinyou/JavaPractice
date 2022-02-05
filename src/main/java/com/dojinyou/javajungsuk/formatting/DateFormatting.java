945aaea2ac4cbecdbb5f796c80b5102e1f44aac5
dojinyou <djyou128@gmail.com>
2022-02-05 15:39:16 +0900

    Feat: Date Formatting with Calendar
    Calendar 객체를 이용한 날짜시간 formatting 진행
    Calendar 객체는 SimpleDateFormat으로 formatting이 되지 않기 때문에
    Date 객체로 변환하여 사용해야된다.

--- src/main/java/com/dojinyou/javajungsuk/formatting/DateFormatting.java
+++ src/main/java/com/dojinyou/javajungsuk/formatting/DateFormatting.java
@@ -1,11 +1,14 @@
 package com.dojinyou.javajungsuk.formatting;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateFormatting {
     public static void main(String[] args) {
         Date today = new Date();
+        Calendar cal = Calendar.getInstance();
+        Date todayCal = cal.getTime();
 
         SimpleDateFormat sdf1,sdf2,sdf3,sdf4;
 
@@ -15,12 +18,16 @@ public class DateFormatting {
         sdf4 = new SimpleDateFormat("yyyy년 MM월 dd일 E요일 hh:mm:ss.SSS");
 
         System.out.println("sdf1 = " + sdf1.format(today));
+//      System.out.println("sdf1 = " + sdf1.format(todayCal));
 //      sdf1 = 2022-02-05
         System.out.println("sdf2 = " + sdf2.format(today));
+//      System.out.println("sdf2 = " + sdf2.format(todayCal));
 //      sdf2 = 2022년 02월 05일 오후 03:29:41
         System.out.println("sdf3 = " + sdf3.format(today));
+//      System.out.println("sdf3 = " + sdf3.format(todayCal));
 //      sdf3 = 2022년 02월 05일 15:29:41
         System.out.println("sdf4 = " + sdf4.format(today));
+//      System.out.println("sdf4 = " + sdf4.format(todayCal));
 //      sdf4 = 2022년 02월 05일 토요일 03:29:41.239
     }
 }
