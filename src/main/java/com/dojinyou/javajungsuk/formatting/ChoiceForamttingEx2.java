7ed1625732e52ce3324ee7321aa55aa3baf4a1e9
dojinyou <djyou128@gmail.com>
2022-02-05 16:22:36 +0900

    Feat: Choice Formatting 관련 예제
    ChoiceFormat을 이용한 예제 작성
    limits와 value을 이용한 실행 방법과 패턴문자열을 이용하는 방법 예제 실행

---
+++ src/main/java/com/dojinyou/javajungsuk/formatting/ChoiceForamttingEx2.java
@@ -0,0 +1,26 @@
+package com.dojinyou.javajungsuk.formatting;
+
+import java.text.ChoiceFormat;
+
+public class ChoiceForamttingEx2 {
+    public static void main(String[] args) {
+        String pattern1 = "60#D|70#C|80#B|90#A";
+        String pattern2 = "60<D|70<C|80<B|90<A";
+
+        ChoiceFormat cf1 = new ChoiceFormat(pattern1);
+        ChoiceFormat cf2 = new ChoiceFormat(pattern2);
+
+        int[] scores = {99,90,80,70,60};
+
+        for (int score : scores) {
+            System.out.println(score+"=> #format : "+cf1.format(score)+", <format : "+cf2.format(score));
+        }
+        /*
+        * 99=> #format : A, <format : A
+        * 90=> #format : A, <format : B
+        * 80=> #format : B, <format : C
+        * 70=> #format : C, <format : D
+        * 60=> #format : D, <format : D
+        * */
+    }
+}
---
+++ src/main/java/com/dojinyou/javajungsuk/formatting/ChoiceFormattingEx.java
@@ -0,0 +1,27 @@
+package com.dojinyou.javajungsuk.formatting;
+
+import java.text.ChoiceFormat;
+
+public class ChoiceFormattingEx {
+    public static void main(String[] args) {
+        double[] limits = {60,70,80,90};
+        String[] values = {"D","C","B","A"};
+        ChoiceFormat cf = new ChoiceFormat(limits,values);
+
+        int[] scores = {100,95,88,80,52,60,70};
+
+        for (int score : scores) {
+            System.out.println(score+":"+cf.format(score));
+        }
+        /* limits 범위에 따라 value가 할당되는 것을 알 수 있음.
+        100:A
+        95:A
+        88:B
+        80:B
+        52:D
+        60:D
+        70:C
+        */
+
+    }
+}
