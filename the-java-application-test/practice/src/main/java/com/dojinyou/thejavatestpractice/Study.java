d79da20baa19524b40b4fa84f657ef8f4ad451c7
dojinyou <djyou128@gmail.com>
2022-04-16 15:59:05 +0900

    :sparkles: feac: test를 위한 기능 및 클래스 구현

--- the-java-application-test/practice/src/main/java/com/dojinyou/thejavatestpractice/Study.java
+++ the-java-application-test/practice/src/main/java/com/dojinyou/thejavatestpractice/Study.java
@@ -1,4 +1,21 @@
 package com.dojinyou.thejavatestpractice;
 
 public class Study {
+    private StudyStatus status;
+    private int numOfMaximumMembers;
+
+    public Study(int numOfMaximumMembers) {
+        if(numOfMaximumMembers <= 0) {
+            throw new IllegalArgumentException("number of maxnimum member is must bigger than 0");
+        }
+        this.numOfMaximumMembers = numOfMaximumMembers;
+    }
+
+    public StudyStatus getStatus() {
+        return this.status;
+    }
+
+    public int getNumOfMaximumMembers() {
+        return this.numOfMaximumMembers;
+    }
 }
---
+++ the-java-application-test/practice/src/main/java/com/dojinyou/thejavatestpractice/StudyStatus.java
@@ -0,0 +1,5 @@
+package com.dojinyou.thejavatestpractice;
+
+public enum StudyStatus {
+    DRAFT, STARTED, ENDED;
+}
